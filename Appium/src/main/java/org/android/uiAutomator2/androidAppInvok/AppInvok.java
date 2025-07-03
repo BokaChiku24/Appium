535ba53913d36677c24fca839a330a284183a832
kunalchavan <kunal24chavan@gmail.com>
2025-07-03 13:44:23 +0530

    Start Activity

---
+++ Appium/src/main/java/org/android/uiAutomator2/androidAppInvok/AppInvok.java
@@ -0,0 +1,33 @@
+package org.android.uiAutomator2.androidAppInvok;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+public class AppInvok extends BaseTest {
+
+	// @SuppressWarnings("deprecation")
+	@Test
+	public void appInvoke() {
+		// App Package - Full App with Package and Activities
+		// App Activity - Specific APp Activity
+
+		/*
+		 * Activity activity = new Activity("io.appium.android.apis",
+		 * "io.appium.android.apis.preference.PreferenceDependencies");
+		 * driver.startActivity(activity);
+		 */
+		((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of("intent",
+				"io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"));
+		try {
+			Thread.sleep(Duration.ofSeconds(10));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Appium/src/main/java/org/android/uiAutomator2/androidMiscellaneousActivity/LandscapeMode.java
@@ -0,0 +1,36 @@
+package org.android.uiAutomator2.androidMiscellaneousActivity;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+
+public class LandscapeMode extends BaseTest {
+	@Test
+	public void landTest() {
+		// Rotation of the device from landscape to portrait mode
+		// DeviceRotation landScape = new DeviceRotation(0, 90, 0);
+		// driver.rotate(landScape);
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		// Syntax of Xpath: //tagName[@attribute='value'] OR //tagName
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+
+		// Rotation of the device to landscape mode
+		DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+		driver.rotate(landScape);
+
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(alertTitle, "WiFi settings");
+		driver.findElement(By.id("android:id/edit")).sendKeys("Kunal WiFi");
+		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+
+		// Rotation of the device from landscape to portrait mode
+		landScape = new DeviceRotation(0, 90, 0);
+		driver.rotate(landScape);
+
+	}
+}
